ba6df591629234ab945080685ed4fb105f5d1024
Maks <maks.yaremenko@gmail.com>
2017-04-15 18:08:07 +0300

    video-7

--- _gulpfile.js
+++ _gulpfile.js
@@ -1,57 +1,33 @@
 'use strict';
 
 const gulp = require('gulp');
-const stylus = require('gulp-stylus'); // для конвертации стилус в ксс
 const concat = require('gulp-concat'); // для объединения файлов в 1
 const debug = require('gulp-debug'); // пропускает все через себя и выводит что происходит
-const sourcemaps = require('gulp-sourcemaps'); // отображает что было и что стало
-const gulpIf = require('gulp-if'); // позволяет в зависимости от условия пропускать или запускать поток
-const del = require('del'); // для удаления директорий
-const newer = require('gulp-newer'); // для пропуска в обработку только обновленных файлов
-
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
-// не забывать возвращать результат или колбак
+const autoprefixer = require('gulp-autoprefixer');
+const remember = require('gulp-remember'); // добавляет файлы если они были пропущены при контроле версий
+const path = require('path');
+const cached = require('cached');
 
 gulp.task('styles', function (callback) {
 
-	console.log(require('stylus/lib/parser').cache);
-
-	//return gulp.src('frontend/**/*.styl')
-	//return gulp.src('frontend/styles/**/main.styl', {base: 'frontend'})
-	return gulp.src('frontend/styles/**/main.styl')
-		.pipe(gulpIf(isDevelopment, sourcemaps.init()))  // file.sourceMap
-		//.pipe(debug({title: 'src'}))
-		.pipe(stylus()) // кеширует результат после первого запуска
-		//.pipe(debug({title: 'stylus'}))
-		//.pipe(concat('app.css'))
-		//.pipe(debug({title: 'concat'}))
-		.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
+	//return gulp.src('frontend/**/*.css', {since: gulp.lastRun('styles')})
+	return gulp.src('frontend/**/*.css', {since: gulp.lastRun('styles')})
+		.pipe(cached('styles')) // смотрит на содержимое файла а не на дату модификации как since
+								// since работает быстрее т.к смотрит дату, а не читает файл
+		.pipe(autoprefixer())
+		.pipe(remember('styles')) // запоминает файлы которые через него прошли
+		.pipe(concat('all.css'))
 		.pipe(gulp.dest('public'))
 
 });
 
-gulp.task('clean', function () {
-	return del('public'); // возвр промис и гал понимает что задача завершена
-});
-
-gulp.task('assets', function () {
-	return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')}) // будет проверять файлы которые
-																  // были изменены после последнего запуска
-			.pipe(newer('public')) // проверяет если такой файл уже есть с такой же версией то не пропускает его
-			.pipe(debug({title: 'debug-assets'}))
-			.pipe(gulp.dest('public'));
-})
-
-gulp.task('build', gulp.series(
-		'clean', 
-		gulp.parallel('styles', 'assets')));
-
 gulp.task('watch', function () {
-	gulp.watch('frontend/styles/**/*.*', gulp.series('styles')); // нужно указывать или серию или параллель
-	gulp.watch('frontend/assets/**/*.*', gulp.series('assets')); 
+	gulp.watch('frontend/styles/**/*.css', gulp.series('styles')).on('unlink', function (filepath) {
+		remember.forget('styles', path.resolve(filepath)); // чтоб забыть удаленные файлы
+		delete cached.caches.styles[path.resolve(filepath)]
+	})
 });
 
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('dev', gulp.series('styles', 'watch'));
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,74 @@
 'use strict';
 
+// livereload 
+// tiny-lr
+
+//  browser-sync
+//   ^        ^
+// Chrome  Safari
 const gulp = require('gulp');
-const concat = require('gulp-concat'); // для объединения файлов в 1
+const stylus = require('gulp-stylus'); // для конвертации стилус в ксс
+//const concat = require('gulp-concat'); // для объединения файлов в 1
 const debug = require('gulp-debug'); // пропускает все через себя и выводит что происходит
-const autoprefixer = require('gulp-autoprefixer');
-const remember = require('gulp-remember'); // добавляет файлы если они были пропущены при контроле версий
-const path = require('path');
-const cached = require('cached');
+const sourcemaps = require('gulp-sourcemaps'); // отображает что было и что стало
+const gulpIf = require('gulp-if'); // позволяет в зависимости от условия пропускать или запускать поток
+const del = require('del'); // для удаления директорий
+//const newer = require('gulp-newer'); // для пропуска в обработку только обновленных файлов
+const browserSync = require('browser-sync').create();
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+// не забывать возвращать результат или колбак
 
 gulp.task('styles', function (callback) {
 
-	//return gulp.src('frontend/**/*.css', {since: gulp.lastRun('styles')})
-	return gulp.src('frontend/**/*.css', {since: gulp.lastRun('styles')})
-		.pipe(cached('styles')) // смотрит на содержимое файла а не на дату модификации как since
-								// since работает быстрее т.к смотрит дату, а не читает файл
-		.pipe(autoprefixer())
-		.pipe(remember('styles')) // запоминает файлы которые через него прошли
-		.pipe(concat('all.css'))
+	//console.log(require('stylus/lib/parser').cache);
+
+	//return gulp.src('frontend/**/*.styl')
+	//return gulp.src('frontend/styles/**/main.styl', {base: 'frontend'})
+	return gulp.src('frontend/styles/**/main.styl')
+		.pipe(gulpIf(isDevelopment, sourcemaps.init()))  // file.sourceMap
+		//.pipe(debug({title: 'src'}))
+		.pipe(stylus()) // кеширует результат после первого запуска
+		//.pipe(debug({title: 'stylus'}))
+		//.pipe(concat('app.css'))
+		//.pipe(debug({title: 'concat'}))
+		.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
 		.pipe(gulp.dest('public'))
 
 });
 
-gulp.task('watch', function () {
-	gulp.watch('frontend/styles/**/*.css', gulp.series('styles')).on('unlink', function (filepath) {
-		remember.forget('styles', path.resolve(filepath)); // чтоб забыть удаленные файлы
-		delete cached.caches.styles[path.resolve(filepath)]
-	})
+gulp.task('clean', function () {
+	return del('public'); // возвр промис и гал понимает что задача завершена
 });
 
-gulp.task('dev', gulp.series('styles', 'watch'));
+gulp.task('assets', function () {
+	return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')}) // будет проверять файлы которые
+																  // были изменены после последнего запуска
+			//.pipe(newer('public')) // проверяет если такой файл уже есть с такой же версией то не пропускает его
+			.pipe(debug({title: 'debug-assets'}))
+			.pipe(gulp.dest('public'));
+})
 
+gulp.task('build', gulp.series(
+		'clean', 
+		gulp.parallel('styles', 'assets')));
 
+gulp.task('watch', function () {
+	gulp.watch('frontend/styles/**/*.*', gulp.series('styles')); // нужно указывать или серию или параллель
+	gulp.watch('frontend/assets/**/*.*', gulp.series('assets')); 
+});
+
+gulp.task('serve', function () {
+	browserSync.init({
+		server: 'public' // куда вклеивать скрипт
+		// так же можно делать проксирование
+	});
+
+	browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev', 
+	gulp.series('build', gulp.parallel('watch', 'serve'))
+	// делаем паралель т.к вотч и серв никогда не завершается
+);
